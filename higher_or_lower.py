91cc1ef393a9ed0a01d8793f3d41fecbf23cfe3b
James Thompson <jthompson@zipcoders-MacBook-Pro.local>
2020-03-05 23:54:53 -0500

    updated higher_or_lower

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -1,20 +1,22 @@
 from random import randrange
 
 def higher_or_lower():
-	
+  
   random_num = randrange(10)
 
   user_guess = int(input("Pick a number from 1-10: "))
   print("Random number was " + str(random_num))
 
-  if random_num < user_guess:
+  if random_num > user_guess:
+    print("Your guess was " + str(user_guess))
     print("Too low")
 
-  elif random_num > user_guess:
+  elif random_num < user_guess:
+    print("Your guess was " + str(user_guess))
     print("Too high")
 
   elif random_num == user_guess:
-    print("You guessed it!")
+    print("Correct! Your guess was " + str(user_guess) + " you guessed correct!")
 
 """
 Random number is generated. User inputs a number and it is compared to random
